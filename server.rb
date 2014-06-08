867eebf86fb3877d574250590d516282a965c0b7
Julie Kramer <thejuliekramer@gmail.com>
2014-06-08 19:18:14 -0400

    updated website

--- server.rb
+++ server.rb
@@ -4,5 +4,9 @@ require 'sinatra'
 		 send_file File.expand_path('index.html', settings.public_folder)
 	end
 
+	get "/fillmurray" do
+		 send_file File.expand_path('views/fillmurray/fillmurray.html', settings.public_folder)
+	end
+
 
 	
