99e745941a7858f3a50bbffe923795f33d3155a9
Julie Kramer <thejuliekramer@gmail.com>
2014-07-07 15:35:42 -0400

    rebuilt portfolio site on rails

--- server.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-
-	get "/" do
-		 send_file File.expand_path('index.html', settings.public_folder)
-	end
-
-	get "/fillmurray" do
-		 send_file File.expand_path('fillmurray.html', settings.public_folder)
-	end
